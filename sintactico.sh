3bf0aabad5bcef685c8df67832745f69dbe9b66f
Mariani88 <pm_mariani@hotmail.com>
2015-10-27 00:04:48 -0300

    agrego archivos para realizar integracion continua

--- sintactico.sh
+++ sintactico.sh
@@ -1,13 +1,3 @@
-mkdir archivosDelCompilador
 bison -d sintactico.y
 flex lexico.l
-
-
-mv  lex.yy.c /home/matias/Escritorio/practica/archivosDelCompilador/
-mv  sintactico.tab.c /home/matias/Escritorio/practica/archivosDelCompilador/
-mv  sintactico.tab.h /home/matias/Escritorio/practica/archivosDelCompilador/
-
-cd archivosDelCompilador
 gcc -o analizador lex.yy.c sintactico.tab.c
-
-./analizador
